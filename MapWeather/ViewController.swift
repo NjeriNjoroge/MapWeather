d6ab108442358a8952396dbcfb61e97fe57bf5e1
NjeriNjoroge <grace.njoroge@incentro.com>
2020-11-11 12:01:18 +0300

    initial commit

---
+++ MapWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapWeather
+//
+//  Created by Grace Njoroge on 11/11/2020.
+//  Copyright © 2020 Grace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
